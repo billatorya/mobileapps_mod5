833f45654dd5f206e5144fd5030d57e0cd5b9888
billatorya <widiaprasetia26@gmail.com>
2021-04-28 16:22:27 +0800

    change jawab 1

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 // }
  
 void main() => runApp(MyApp());
-
+ 
 class MyApp extends StatefulWidget {
  @override
  State<StatefulWidget> createState() {
@@ -13,43 +13,45 @@ class MyApp extends StatefulWidget {
    return _MyAppState();
  }
 }
-
-class _MyAppState extends State<MyApp> {
- var questionIndex = 0;
  
- void answerQuestion() {
-   questionIndex = questionIndex + 1;
-   print(questionIndex);
+class _MyAppState extends State<MyApp> {
+ var _questionIndex = 0;
+ 
+ void _answerQuestion() {
+   setState(() {
+     _questionIndex = _questionIndex + 1;
+   });
+   print(_questionIndex);
  }
  
  @override
  Widget build(BuildContext context) {
    var questions = [
-      'Apa warna favorit anda?',
+     'Apa warna favorit anda?',
      'apa peliharaan favorit anda?',
    ];
    return MaterialApp(
      home: Scaffold(
        appBar: AppBar(
-         title: Text('Aplikasi Quiz'),
+         title: Text('Aplikasi quiz'),
        ),
        body: Column(
          children: [
            Text(
-             questions[questionIndex],
-           ),
+             questions[_questionIndex],
+           ),+
+           
            ElevatedButton(
              child: Text('jawab 1'),
-             onPressed: answerQuestion,
+             onPressed: _answerQuestion,
            ),
            ElevatedButton(
              child: Text('jawab 2'),
-             onPressed: () => print('jawab 2 dipilih!'), // ini akan muncul pada debug console di visual code
+             onPressed: () => print('jawab 2 dipilih!'),
            ),
            ElevatedButton(
              child: Text('jawab 3'),
              onPressed: () {
-               // ...
                print('jawab 3 dipilih');
              },
            ),
